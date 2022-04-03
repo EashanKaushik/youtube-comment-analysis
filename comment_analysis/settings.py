6f524fb3d957cc323a1911c5920193ab64f6ac07
EashanKaushik <Eashank16@gmail.com>
2022-04-03 00:16:23 -0400

    heroku error

--- comment_analysis/settings.py
+++ comment_analysis/settings.py
@@ -21,7 +21,6 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-print(BASE_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -126,11 +125,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATIC_URL = "static/"
+# this is directory name where collectstatic files command will put your app level static files
+STATIC_ROOT = 'staticfiles'
 
-STATICFILES_DIRS = [BASE_DIR / "static"]
+# this is directory paths where you have to put your project level static files
+# you can put multiple folders here
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 

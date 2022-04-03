49c2f7f6a198f875d525ea0eec33a380e90e010e
EashanKaushik <Eashank16@gmail.com>
2022-04-03 00:01:02 -0400

    heroku error

--- comment_analysis/settings.py
+++ comment_analysis/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "static/"
 
-STATICFILES_DIRS = = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
